714fd8169f165f9532790239efe6e61a0f55f7c1
riyad5541 <abdullahalriyad5541@gmail.com>
2023-07-06 13:20:37 +0600

    trable-buddy completed

--- app.js
+++ app.js
@@ -37,6 +37,9 @@ var bikeObject = {
 
 
 
+  const services = [carObject, busObject, bikeObject];
+
+
 
 
 
@@ -45,6 +48,7 @@ function displayService(service){
 
     const mainSection = document.getElementById('main-section')
 
+    const stingifiedObj = JSON.stringify(service)
     const div = document.createElement("div")
     div.innerHTML = `
     <div class="card mx-auto mt-3" style="max-width: 800px;">
@@ -57,6 +61,10 @@ function displayService(service){
                   <h5 class="card-title">${service.vehicle}</h5>
                   <p class="card-text">${service.description}</p>
                   <p class="card-text"><small class="text-muted">Fare per kilo ${service.farePerKilo}</small><small class="text-muted">Capacity${service.capacity}</small></p>
+                  <!-- Button trigger modal -->
+             <button type="button" class="btn btn-primary" data-bs-toggle="modal" onclick='handleBooking(${stingifiedObj})'data-bs-target="#exampleModal">
+                     Launch demo modal
+             </button>
                 </div>
               </div>
             </div>
@@ -65,9 +73,71 @@ function displayService(service){
 
     mainSection.appendChild(div);
 
-    console.log(service)
+    // console.log(service)
 }
 
-displayService(carObject)
-displayService(bikeObject)
-displayService(busObject)
+function displayAllArry(arr){
+  for(let i = 0; i < arr.length; i++){
+    const element = arr[i];
+    displayService(element)
+
+  }
+}
+
+displayAllArry(services)
+
+
+// handle booking
+
+function handleBooking(obj){
+  const modalBody = document.getElementById('modal-body');
+
+  const stingifiedObj = JSON.stringify(obj)
+
+  modalBody.innerHTML = `
+  <div class="card mx-auto" style="width: 18rem;">
+  <img src=${obj.imageUrl} class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Vehicle Mood : ${obj.vehicle}</h5>
+    <p class="card-text">${obj.description}</p>
+    <p class="card-text"><small class="text-muted">Fare per kilo ${obj.farePerKilo}</small><small class="text-muted">Capacity${obj.capacity}</small></p>
+    <P>Fare: <small class="text-muted" id="fare"></small></p>
+    <P>Tax: <small class="text-muted"></small></p>
+    <P>Total-Cost: <small class="text-muted"></small></p>
+    <div class="d-flex flex-column" role="search">
+              <input class="form-control m-2" id="distance-input" type="number" placeholder="How many kilo to go?" aria-label="Search">
+              <input class="form-control m-2" id="quantity-input" type="number" placeholder="how many vehicle you need?" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit" onclick='calculateCost(${stingifiedObj})'>Submit</button>
+    </div>
+  </div>
+</div>
+  `
+  
+}
+
+
+
+function calculateCost(obj){
+  const quantity = document.getElementById('quantity-input').value;
+  const distance = document.getElementById('distance-input').value;
+
+  const fareDiv = document.getElementById('fare');
+
+  fareDiv.innerHTML = quantity * distance * obj.farePerKilo
+  console.log(fareDiv)
+}
+
+
+document.getElementById('search-btn').addEventListener('click',function(){
+  const value = document.getElementById('search').value;
+  
+  for(let i = 0; i < services.length; i++){
+    const element = services[i];
+    if(value.toLowerCase() == element.vehicle.toLowerCase()){
+      document.getElementById('main-section').innerHTML="";
+      displayService(element);
+      return;
+    }
+  }
+  alert("Nothing found ")
+})
